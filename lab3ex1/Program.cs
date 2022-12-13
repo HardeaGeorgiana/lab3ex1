0f6ae8e67dd7de815ee713c592d0cdb70f46fad4
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-12-13 22:40:34 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace lab3ex1
+{
+    /*  L3E1. Scrieti o functie care va calcula suma cifrelor unui numar.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar ");
+            int number = int.Parse(Console.ReadLine());
+            int suma = SumaCifrelor(number);
+            Console.WriteLine($"Suma cifrelor a numarului este {suma}");
+        }
+        static int SumaCifrelor( int number)
+        {
+            int SumaCifrelor = 0;
+            while (number != 0)
+            {
+                SumaCifrelor += number % 10;
+                number /= 10;
+            }
+            return SumaCifrelor;
+        }
+        
+    }
+}
